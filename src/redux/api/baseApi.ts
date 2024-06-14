85c0a0c29128996bd2e77a5d60c6eb42849577a6
MridulSheikh <prince9nazir@gmail.com>
2024-06-14 19:48:45 +0600

    add live url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://event-pride-server.vercel.app/api/v1",
     // prepareHeaders: (headers, { getState }) => {
     //   const { auth } = getState() as { auth: TAuthState };
     //   const authHeader = "bearer" + " " + auth.token;
